cd1a32ab8b06efd9e1f41fa40bd7384d3dd6533c
Eswar797 <eswarnarayana797@gmail.com>
2024-09-19 01:31:04 +0530

    new

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // MongoDB connection
 const mongoURI = process.env.MONGO_URI;
